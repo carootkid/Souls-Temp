aac599c0f851f6197139d26aa9b4c64b75c74bc4
ChaseB62 <chaseb62@educbe.ca>
2024-01-25 16:40:43 -0700

    Making dat movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,13 +13,32 @@ public class PlayerMovement : MonoBehaviour
     public bool isGrounded;
     public LayerMask ground;
     public GroundDetector groundDetectorScript;
+    public Transform orientation;
 
     public float jumpForce = 10f;
+
+    public float walkSpeed = 10f;
 
     void Update()
     {
         if(groundDetectorScript.grounded && Input.GetKeyDown(jump)){
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        
+        if(Input.GetKey(forward)){
+            rb.AddForce(orientation.forward * walkSpeed);
+        }
+
+        if(Input.GetKey(right)){
+            rb.AddForce(orientation.right * walkSpeed);
+        }
+
+        if(Input.GetKey(left)){
+            rb.AddForce(orientation.right * (walkSpeed * -1));
+        }
+
+        if(Input.GetKey(backward)){
+            rb.AddForce(orientation.forward * (walkSpeed * -1));
         }
     }
 }
