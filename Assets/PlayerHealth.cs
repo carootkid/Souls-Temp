cd023b74c42314134131ff2969a5db1da0cf698b
Destroyer10153 <Aidenmoss07@gmail.com>
2024-01-26 23:38:56 -0700

    NOMOREBOOBIES!

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int maxPotions;
+    public int currentPotions;  // Track the current number of potions the player has
     public float playerHealth = 100f;
     public float maxHealth = 100f;
 
-    public void TakeDamage(float damage){
+    private void Start()
+    {
+        currentPotions = maxPotions;  // Start with the maximum number of potions
+    }
+
+    public void TakeDamage(float damage)
+    {
         playerHealth -= damage;
         Debug.Log("Hit");
 
-        if(playerHealth < 0){
+        if (playerHealth <= 0)
+        {
             Debug.Log("GONE");
         }
+    }
 
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            UseHealingPotion();
+        }
+    }
+
+    void UseHealingPotion()
+    {
+        if (currentPotions > 0)
+        {
+          
+            playerHealth += 20f;  
+            currentPotions--;
+
+          
+            playerHealth = Mathf.Min(playerHealth, maxHealth);
+
+            Debug.Log("Used Healing Potion. Current Health: " + playerHealth + ", Potions Left: " + currentPotions);
+        }
+        else
+        {
+            Debug.Log("No Healing Potions Left");
+        }
     }
 }
