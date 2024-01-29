fdef0f2ce372fae5f52970972c2fcdc8e8f46ced
ChaseB62 <chaseb62@educbe.ca>
2024-01-29 13:48:59 -0700

    poop monster

--- Assets/EnemyScriptReal.cs
+++ Assets/EnemyScriptReal.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class EnemyScriptReal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int Temphealth = 50;
+    public float moveSpeed = 5f;
+    public float detectionRange = 10f;
+    public float attackRange = 3f;
+    public int damageAmount = 10;
+    public float attackCooldown = 2f;
+
+    private GameObject player;
+    private float lastAttackTime;
+    public LevelUpScript levelUpScript;
+    public Animator enemyAnimator;
+    public Collider attackCollider;
+
     void Start()
+    {
+
+        lastAttackTime = -attackCooldown;
+    }
+
+    void Update()
+    {
+        if (Temphealth > 0)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+
+            if (player != null)
+            {
+                float distanceToPlayer = Vector3.Distance(new Vector3(transform.position.x, 0f, transform.position.z),
+                new Vector3(player.transform.position.x, 0f, player.transform.position.z));
+
+                if (distanceToPlayer <= detectionRange)
+                {
+                    if (distanceToPlayer <= attackRange && Time.time - lastAttackTime > attackCooldown)
+                    {
+                        AttackPlayer();
+                        lastAttackTime = Time.time;
+                    }
+                    else
+                    {
+                        MoveTowardsPlayer(player, distanceToPlayer);
+                    }
+                }
+                else
+                {
+                    StopMoving();
+                }
+            }
+        }
+        else
+        {
+            Die();
+            levelUpScript.playerEXP = levelUpScript.playerEXP + 50;
+        }
+    }
+
+    void MoveTowardsPlayer(GameObject player, float distanceToPlayer)
+        {
+            if (distanceToPlayer > attackRange)
+            {
+                enemyAnimator.SetBool("idle", false);
+                Vector3 playerPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+                Vector3 direction = (playerPosition - transform.position).normalized;
+                transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
+                transform.LookAt(playerPosition);
+            } else {
+                enemyAnimator.SetBool("idle", true);
+            }
+        }
+
+    void StopMoving()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void AttackPlayer()
     {
-        
+        enemyAnimator.SetTrigger("attack");
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        Temphealth -= damageAmount;
+        Debug.Log("Took Damage!");
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/HurtPlayer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour
+{
+    public LayerMask playerLayer;
+    
+    public GameObject hitEffectPlayer;
+    
+    public int attackDamage = 25;
+    void OnTriggerEnter(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & playerLayer) != 0)
+        {
+            Vector3 hitPoint = other.transform.position;
+            Vector3 hitNormal = other.transform.forward;
+            RaycastHit hitInfo;
+            Debug.Log("Hit Enemy");
+            
+            if (other.TryGetComponent(out Collider collider))
+            {
+                if (collider.Raycast(new Ray(transform.position, transform.forward), out hitInfo, Mathf.Infinity))
+                {
+                    hitPoint = hitInfo.point;
+                    hitNormal = hitInfo.normal;
+                }
+            }
+
+            SpawnHitEffect(hitPoint, hitNormal, hitEffectPlayer);
+            
+            other.SendMessage("TakeDamage", attackDamage, SendMessageOptions.DontRequireReceiver);
+
+            Debug.Log("Hit Player");
+            
+        }
+
+    }
+
+    private void SpawnHitEffect(Vector3 position, Vector3 normal, GameObject effectPrefab)
+    {
+        Quaternion rotation = Quaternion.LookRotation(normal);
+
+        Instantiate(effectPrefab, position, rotation);
+    }
+}
